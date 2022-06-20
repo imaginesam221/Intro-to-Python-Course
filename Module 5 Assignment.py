c280254cdfe71e5bfdee0aa4d8cf09518c1c3b58
imaginesam221 <107841108+imaginesam221@users.noreply.github.com>
2022-06-20 04:09:42 -0400

    Add files via upload
    Module 5 Assignment code

---
+++ Module 5 Assignment.py
@@ -0,0 +1,45 @@
+from graphics import *
+
+class Point: 
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y 
+
+class Rectangle: 
+    def __init__(self, posn, w, h): 
+        self.corner = posn 
+        self.width = w 
+        self.height = h 
+
+def __str__(self): 
+    return "({0}, {1}, {2})".format(self.corner, self.width, self.height) 
+    
+def create_rectangle(x, y, width, height): 
+    posn = Point(x,y) 
+    rect = Rectangle(posn, width, height) 
+    return rect 
+
+def str_rectangle(rect): 
+    return (rect.corner.x, rect.corner.y, rect.width, rect.height) 
+
+def shift_rectangle(rect, dx, dy): 
+    rect.corner.x += dx 
+    rect.corner.y += dy 
+
+def offset_rectangle(rect, dx, dy): 
+    x = rect.corner.x + dx 
+    y = rect.corner.y + dy 
+    return(create_rectangle(x, y, rect.width, rect.height)) 
+
+
+#Code Test for Assignment 5
+r1 = create_rectangle(10, 20, 30, 40) 
+print(str_rectangle(r1))
+
+shift_rectangle(r1, -10, -20)
+print(str_rectangle(r1)) 
+
+print(str_rectangle(r1)) 
+
+r2 = offset_rectangle(r1, 100, 100) 
+print(str_rectangle(r2)) 
